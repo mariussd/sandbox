3b2b6cd976460c9b5897264de276d51a0d0e7488
mariussd <marius.s.dreyer@gmail.com>
2023-10-23 08:57:14 +0200

    update title and favicon

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,12 +1,11 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { LinksFunction, MetaFunction } from "@remix-run/node";
 import { Link } from "@remix-run/react";
 
-import { useOptionalUser } from "~/utils";
+export const meta: MetaFunction = () => [{ title: "mariussd" }];
 
-export const meta: MetaFunction = () => [{ title: "Remix Notes" }];
+export const links: LinksFunction = () => [{rel: "icon", href: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👹</text></svg>'}]
 
 export default function Index() {
-  const user = useOptionalUser();
   return (
     <main className="relative min-h-screen bg-white sm:flex sm:items-center sm:justify-center">
       <div className="flex flex-col items-center">
